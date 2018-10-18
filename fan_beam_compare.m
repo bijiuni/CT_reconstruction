d9e8fa8f5e9ab729a0989ce4222c8664194548f3
Zach Lyu <zach_lyu@berkeley.edu>
2018-10-17 18:42:49 -0700

    Update fan_beam_compare.m

--- fan_beam_compare.m
+++ fan_beam_compare.m
@@ -1,7 +1,14 @@
+%{ Compare the performance of pencil-beam and fan-beam CT reconstruction
+	 
+	   % This is a simple program with no function defined
+%}
+
+% Calculate pencil-beam and fan-beam result for a single column
 I = phantom(256); D = 200; dtheta = 1; [Farc,FposArcDeg,Fangles] =  fanbeam(I,D,'FanSensorGeometry','arc','FanRotationIncrement',dtheta); 
  
 FposArc = D*tan(FposArcDeg*pi/180); 
  
 [R,Rpos]=radon(I,Fangles); 
- 
+
+% Plot the result and compare the performance
 figure idx = find(Fangles==90); plot(Rpos,R(:,idx),FposArc,Farc(:,idx)) legend('Radon','Arc') 
