cd64ac8239c60cf1ce76cf13849c19aa866e448c
Zach Lyu <zach_lyu@berkeley.edu>
2018-10-17 20:03:18 -0700

    Update own_radon.m

--- own_radon.m
+++ own_radon.m
@@ -1,3 +1,9 @@
+% Realizing radon transform without using built-in fuction
+% param img
+%        the original image to be radon transformed
+% param theta
+%        the interval of the projection angles
+
 function processed_img = own_radon(img, theta) 
  
 [n_row, n_column] = size(img); Dia = sqrt(n_row + n_column); row_Pad = ceil(Dia - n_row) + 2; col_Pad = ceil(Dia - n_column) + 2; pad = zeros(n_row+row_Pad, n_column+col_Pad); pad(ceil(row_Pad/2):(ceil(row_Pad/2)+n_row-1), ...     ceil(col_Pad/2):(ceil(col_Pad/2)+n_column-1)) = img; 
